679ab9bed637d6e58e80d0b4488535edeaaa483e
Jennifer Kim <jkim8191@gmail.com>
2019-02-12 17:53:40 +0000

    first commit

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in worlds_best_restaurants.gemspec
+gemspec
---
+++ lib/worlds_best_restaurants.rb
@@ -0,0 +1,6 @@
+require "worlds_best_restaurants/version"
+
+module WorldsBestRestaurants
+  class Error < StandardError; end
+  # Your code goes here...
+end
